94bfc35bd3f0a026f5550d36a71a17a959985fc9
Mukund Kalantri <37955246+mukundkalantri23@users.noreply.github.com>
2019-04-09 15:23:37 +0530

    Update datasetCreator.py

--- datasetCreator.py
+++ datasetCreator.py
@@ -1,3 +1,14 @@
+#On running this code, the user would be asked to enter the ID number and then a camera window will open where a number of 
+#pictures would be taken and processed to get the face in the picture. These pictures would be stored inside the dataSet folder
+#with names according to the ID number provided and the number of picture it was.
+
+#Recommended:- 
+#1. while running this code, it is advisable that only one user should appear infront of the camera or else the dataset
+#   generated would be faulty and so would be the training of the model and final recognizing of the person.
+#2. The user may make different expressions and move their face a little so that the model could be trained with different 
+#   variations of the users face.
+#3. To improve the accuracy, the number of pictures taken per person (see below)  can be increased. (However it would add up space.)
+
 import os
 import cv2
 import numpy as np
@@ -19,7 +30,7 @@ while(True):
         cv2.waitKey(100);
     cv2.imshow("face",img);
     cv2.waitKey(1);
-    if(sampleNum>40):
+    if(sampleNum>40): #Pictures of the user would be taken as long as this condition is true. You might alter this condition if you want.
         break
      
 
